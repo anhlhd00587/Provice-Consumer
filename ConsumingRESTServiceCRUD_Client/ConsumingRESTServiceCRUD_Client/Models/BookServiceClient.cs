3043ac7aaac9a6c923b9f2cea2cccd6843de087f
anhlhd00587 <anhlhd00587@fpt.edu.vn>
2018-12-18 11:52:06 +0700

    nop-bai-tap

---
+++ ConsumingRESTServiceCRUD_Client/ConsumingRESTServiceCRUD_Client/Models/BookServiceClient.cs
@@ -0,0 +1,55 @@
+﻿using ConsumingRESTServiceCRUD_Client.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumingRESTServiceCRUD_Client.Models
+{
+    public class BookServiceClient
+    {
+        Service1Client client = new Service1Client();
+        string BASE_URL = "http://localhost:64638/Service1.svc?wsdl";
+
+        public List<Book> getAllBook()
+        {
+            var list = client.GetBookList().ToList();
+            var rt = new List<Book>();
+            list.ForEach(b => rt.Add(new Book()
+            {
+                BookId = b.BookId,
+                ISDN = b.ISBN,
+                Title = b.Title
+            }));
+            return rt;
+            //var syncClient = new WebClient();
+            //var content = syncClient.DownloadString(BASE_URL + "Books");
+            //var json_serializer = new JavaScriptSerializer();
+            //return json_serializer.Deserialize<List<Book>>(content);
+        }
+        public string AddBook(Book newBook)
+        {
+            var book = new ServiceReference1.Book()
+            {
+                BookId = newBook.BookId,
+                ISBN = newBook.ISDN,
+                Title = newBook.Title
+            };
+            return client.AddBook(book);
+        }
+        public string DeleteBook(int id)
+        {
+            return client.DeleteBook(Convert.ToString(id));
+        }
+        public string edit(Book newBook)
+        {
+            var book = new ServiceReference1.Book()
+            {
+                BookId = newBook.BookId,
+                ISBN = newBook.ISDN,
+                Title = newBook.Title
+            };
+            return client.UpdateBook(book);
+        }
+    }
+}
---
+++ DemoRESTServiceCRUD/DemoRESTServiceCRUD/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DemoRESTServiceCRUD
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        static IBookRepository repository = new BookReponsitory();
+        public string AddBook(Book book)
+        {
+            Book newBook = repository.AddNewBook(book);
+            return "id" + newBook.BookId;
+            //throw new NotImplementedException();
+        }
+
+        public string DeleteBook(string id)
+        {
+            bool delete = repository.DeleteBook(int.Parse(id));
+            if (delete)
+                return "Delete succes";
+            else
+                return "Delete false";
+            //throw new NotImplementedException();
+        }
+
+        public Book getBookById(string id)
+        {
+            return repository.GetBookById(int.Parse(id));
+            //throw new NotImplementedException();
+        }
+
+        public List<Book> GetBookList()
+        {
+            return repository.GetAllBook();
+            //throw new NotImplementedException();
+        }
+
+        public string UpdateBook(Book book)
+        {
+            bool deleted = repository.UpdateBook(book);
+            if (deleted)
+                return "Book with id=" + book.BookId + " Update successfully";
+            else
+                return "Unable to update book with id =" + book.BookId;
+            // throw new NotImplementedException();
+        }
+    }
+}
